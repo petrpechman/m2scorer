f3039b4532b90e51b2a59cbbb7c7c6b3ab518dcc
Petr Pechman <petr@petr.pechman>
2023-04-18 12:14:33 +0200

    Create package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='m2scorer',
+    version='0.1.0',    
+    description='M2scorer for Python3.',
+    url='https://github.com/petrpechman/m2scorer',
+    author='Petr Pechman',
+    author_email='hugo.pechman@outlook.com',
+    packages=['m2scorer'],
+    install_requires=[]
+)
